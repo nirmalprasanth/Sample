662499246e5a3d6a6d42bb05b198e007f699f583
nirmalprasanth <nirmalprasanth96@gmail.com>
2021-11-28 21:14:53 +0530

    First

---
+++ src/test/java/com/test/Sample.java
@@ -0,0 +1,26 @@
+package com.test;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Sample {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demoqa.com/select-menu");
+		List<WebElement> x = driver.findElements(By.tagName("option"));
+		for (WebElement y : x) {
+			if(y.getText().equals("Yellow"))
+			{
+				y.click();
+			}
+		}
+	}
+
+}
